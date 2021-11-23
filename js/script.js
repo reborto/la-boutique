791db413fd5b266015ce55a4c33c5750a98424a0
unknown <tirritoroberto@gmail.com>
2021-11-23 17:52:56 +0100

    immagine_hero

--- js/script.js
+++ js/script.js
@@ -1,44 +1,76 @@
-// import { products } from "./products.js";   <== importare la lista prodotti in modo locale
-
 function createProduct(parent, imgUrl, productTitle, textPrice) {
-    const product = document.createElement("div");
-    product.className = "product";
-  
-    createImg(product, imgUrl, productTitle);
-    createText(product, productTitle, textPrice);
-    parent.appendChild(product);
+  const product = document.createElement("div");
+  product.className = "product";
+
+  createImg(product, imgUrl, productTitle);
+  createText(product, productTitle, textPrice);
+  parent.appendChild(product);
+}
+
+function createImg(parent, imgUrl, productTitle) {
+  const image = document.createElement("img");
+  image.src = imgUrl;
+  image.alt = productTitle;
+
+  parent.appendChild(image);
+}
+
+function createText(parent, productTitle, textPrice) {
+  const title = document.createElement("h4");
+  title.textContent = productTitle;
+
+  const price = document.createElement("strong");
+  price.textContent = `${textPrice} $`;
+
+  parent.append(title, price);
+}
+
+// fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
+//   .then((response) => response.json())
+//   .then((data) => {
+//     products = data;
+//     renderProducts();
+//   });
+
+const wrapperProducts = document.querySelector(".wrapper__products");
+
+function renderProducts(listItems) {
+  listItems.map((product) => {
+    createProduct(wrapperProducts, product.image, product.title, product.price);
+  });
+}
+
+// Async await
+const getProductsList = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+
+  return renderProducts(data);
+};
+
+getProductsList();
+
+
+
+const imgs =[
+"https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+"https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+"https://images.unsplash.com/photo-1462392246754-28dfa2df8e6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" 
+];
+
+
+const change = document.querySelector(".overlay");
+
+let imagesIndex = 0;
+
+let changeImg = setInterval( () => {
+
+  change.style.backgroundImage = `url(${imgs[imagesIndex]})`;
+
+  if (imagesIndex < imgs.length -1) {
+    imagesIndex++
+  } else {
+    imagesIndex = 0
   }
-  
-  function createImg(parent, imgUrl, productTitle) {
-    const image = document.createElement("img");
-    image.src = imgUrl;
-    image.alt = productTitle;
-  
-    parent.appendChild(image);
-  }
-  
-  function createText(parent, productTitle, textPrice) {
-    const title = document.createElement("h4");
-    title.textContent = productTitle;
-  
-    const price = document.createElement("strong");
-    price.textContent = `${textPrice} $`;
-  
-    parent.append(title, price);
-  }
-  
-  fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
-    .then((response) => response.json())
-    .then((data) => {
-      products = data;
-      renderProducts();
-    });
-  
-  let products = [];
-  const wrapperProducts = document.querySelector(".wrapper__products");
-  
-  function renderProducts() {
-    products.map((product) => {
-      createProduct(wrapperProducts, product.image, product.title, product.price);
-    });
-  }
+
+}, 3000)
